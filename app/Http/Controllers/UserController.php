6a2165322fe952a0b8fa81ff30b8b7ce780e62e0
AI1411 <goethe0719@gmail.com>
2020-12-29 22:06:14 +0900

    add FormRequest

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserCreateRequest;
+use App\Http\Requests\UserUpdateRequest;
 use App\User;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,7 +20,7 @@ class UserController extends Controller
         return User::find($id);
     }
 
-    public function store(Request $request)
+    public function store(UserCreateRequest $request)
     {
         $user = User::create([
             'first_name' => $request->input('first_name'),
@@ -30,16 +32,11 @@ class UserController extends Controller
         return response($user, Response::HTTP_CREATED);
     }
 
-    public function update(Request $request, $id)
+    public function update(UserUpdateRequest $request, $id)
     {
         $user = User::find($id);
 
-        $user->update([
-            'first_name' => $request->input('first_name'),
-            'last_name' => $request->input('last_name'),
-            'email' => $request->input('email'),
-            'password' => \Hash::make($request->input('password')),
-        ]);
+        $user->update($request->only(['first_name', 'last_name', 'email']));
 
         return response($user, Response::HTTP_ACCEPTED);
     }
---
+++ app/Http/Requests/UserCreateRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserCreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required|email',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'first_name.required' => '名前は必ず指定してください',
+            'last_name.required' => '苗字は必ず指定してください',
+            'email.required' => 'メールアドレスは必ず指定してください',
+        ];
+    }
+}
