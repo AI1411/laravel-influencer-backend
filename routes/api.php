f266e449616228779e1b7e6f6e9761fabdf3a2ff
AI1411 <goethe0719@gmail.com>
2021-01-08 21:49:13 +0900

    新規登録処理追加

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 Route::post('login', 'AuthController@login');
+Route::post('register', 'AuthController@register');
 
 Route::group(['middleware' => 'auth:api'], function () {
     Route::apiResource('users', 'UserController');
