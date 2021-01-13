24bfb0a7f2e8312788cb9f91e6aade74ee70d889
AI1411 <goethe0719@gmail.com>
2021-01-14 01:49:15 +0900

    initial commit

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,10 @@
 Route::post('login', 'AuthController@login');
 Route::post('register', 'AuthController@register');
 
-Route::group(['middleware' => 'auth:api'], function () {
+Route::group([
+    'middleware' => 'auth:api',
+    'prefix' => 'admin'
+], function () {
     Route::post('logout', 'AuthController@logout');
     Route::get('chart', 'DashboardController@chart');
     Route::get('user', 'UserController@user');
